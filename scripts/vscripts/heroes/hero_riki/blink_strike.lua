2e5dfe9389d7ffc122bed477967428e4056ab455
Kpyrep123 <veber412@gmail.com>
2022-03-21 16:56:10 +0300

    new
    newne

--- scripts/vscripts/heroes/hero_riki/blink_strike.lua
+++ scripts/vscripts/heroes/hero_riki/blink_strike.lua
@@ -72,11 +72,13 @@ end
 	}
 
 	ExecuteOrderFromTable(order)
+
 end
 
 
 
 LinkLuaModifier("modifier_illusion_squal_lua", "heroes/hero_riki/blink_strike.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_outdmg", "heroes/hero_riki/blink_strike.lua", LUA_MODIFIER_MOTION_NONE)
 
 ability_illusion_squal_lua = class({})
 
@@ -93,7 +95,7 @@ function ability_illusion_squal_lua:OnSpellStart()
 end
 
 modifier_illusion_squal_lua = class({})
-function modifier_illusion_squal_lua:IsHidden() return false end
+function modifier_illusion_squal_lua:IsHidden() return true end
 function modifier_illusion_squal_lua:IsPurgable() return false end
 function modifier_illusion_squal_lua:GetTexture() return end
 function modifier_illusion_squal_lua:GetEffectName() return end
@@ -113,18 +115,23 @@ end
 function modifier_illusion_squal_lua:OnIntervalThink()
 	if self.count >= self.count_max then 
 	self:Destroy()
+	local outgoingDamage = self:GetAbility():GetSpecialValueFor("outgoingDamage")
+	local incomingDamage = self:GetAbility():GetSpecialValueFor("incomingDamage")
 	return end
 
 	self.count = self.count + 1
 	local rand_distance = math.random(800, 900)
 	local origin = self:GetParent():GetAbsOrigin() + RandomVector(rand_distance)
-	local illusion = CreateIllusion(self:GetCaster(),self:GetCaster(),origin, 1,0,0)
+	local illusion = CreateIllusion(self:GetCaster(),self:GetCaster(),origin, 0.7,outgoingDamage,incomingDamage)
 	local subab = illusion:FindAbilityByName("blink_strike_datadriven_ills")
 	local point = self:GetParent():GetAbsOrigin()
+	local dmg = illusion:AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_outdmg", {duration = 1})
+	dmg:SetStackCount(self:GetAbility():GetSpecialValueFor("outgoingDamage")) + self:GetCaster():GetTalentValue("special_bonus_unquie_sqwal_dmg")
 	local forward = (point - origin):Normalized()
 		illusion:SetForwardVector(forward)
 		illusion:SetControllableByPlayer(0, false)
 	local p = ParticleManager:CreateParticle("particles/econ/items/void_spirit/void_spirit_immortal_2021/void_spirit_immortal_2021_astral_step_dmg_smoke_b.vpcf", PATTACH_ABSORIGIN_FOLLOW, illusion)
+	print(self.count)
 	if subab then
 		subab:SetHidden(false)
 		Timers:CreateTimer(0.035, function()
@@ -138,4 +145,38 @@ return
 	{
 		
 	}
+end
+
+modifier_outdmg = class({})
+
+modifier_outdmg = class({})
+function modifier_outdmg:IsHidden() return true end
+function modifier_outdmg:IsPurgable() return false end
+function modifier_outdmg:GetTexture() return end
+function modifier_outdmg:GetEffectName() return end
+
+function modifier_outdmg:OnCreated()
+if not IsServer() then return end
+self.out = self:GetAbility():GetSpecialValueFor("outgoingDamage")
+self.inc = self:GetAbility():GetSpecialValueFor("incomingDamage")
+end
+
+function modifier_outdmg:OnRefresh()
+if not IsServer() then return end
+end
+
+function modifier_outdmg:DeclareFunctions()
+return
+	{
+		MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
+		MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE,
+	}
+end
+
+function modifier_outdmg:GetModifierDamageOutgoing_Percentage(  )
+	return -1 * self:GetStackCount()
+end
+
+function modifier_outdmg:GetModifierIncomingDamage_Percentage(  )
+	return self.inc
 end
